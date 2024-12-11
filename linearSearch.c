ff2c296d46856335f59b9db609b005338fdaad1f
uchihalab <hacker.uchihas@gmail.com>
2024-12-11 23:30:47 +0530

    Create linearSearch.c

---
+++ linearSearch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+     int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int target;
+    scanf("%d",&target);
+
+    int result = linearSearch(arr, n, target);
+
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
