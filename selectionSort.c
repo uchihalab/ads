72146b34d42e42783078025a2032fb840d4e3bc6
uchihalab <hacker.uchihas@gmail.com>
2024-12-11 23:34:32 +0530

    Create selectionSort.c

---
+++ selectionSort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    printArray(arr, n);
+    selectionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
