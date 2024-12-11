5d197072c6473b09383f776ab5271903c55ec874
uchihalab <hacker.uchihas@gmail.com>
2024-12-12 00:04:11 +0530

    Create RadixSort.c

---
+++ RadixSort.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+
+void countingSort(int arr[], int n, int exp) {
+    int output[n]; 
+    int count[10] = {0}; 
+
+  
+    for (int i = 0; i < n; i++) {
+        int digit = (arr[i] / exp) % 10;
+        count[digit]++;
+    }
+
+   
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+
+    for (int i = n - 1; i >= 0; i--) {
+        int digit = (arr[i] / exp) % 10;
+        output[count[digit] - 1] = arr[i];
+        count[digit]--;
+    }
+
+  
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+
+void radixSort(int arr[], int n) {
+    int max = getMax(arr, n);
+
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        countingSort(arr, n, exp);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array:\n");
+    printArray(arr, n);
+
+    radixSort(arr, n);
+
+    printf("Sorted array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
