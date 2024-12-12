ea83aec934854e2b606c39fb97dfe40a5e94aa31
uchihalab <hacker.uchihas@gmail.com>
2024-12-12 10:31:15 +0530

    Update RadixSort.c

--- RadixSort.c
+++ RadixSort.c
@@ -4,33 +4,33 @@ void countingSort(int arr[], int n, int exp) {
     int output[n];
     int count[10] = {0};
 
-    // Count occurrences of each digit
+   
     for (int i = 0; i < n; i++)
         count[(arr[i] / exp) % 10]++;
 
-    // Calculate cumulative frequencies
+    
     for (int i = 1; i < 10; i++)
         count[i] += count[i - 1];
 
-    // Place elements in output array
+   
     for (int i = n - 1; i >= 0; i--) {
         output[count[(arr[i] / exp) % 10] - 1] = arr[i];
         count[(arr[i] / exp) % 10]--;
     }
 
-    // Copy sorted elements back to original array
+   
     for (int i = 0; i < n; i++)
         arr[i] = output[i];
 }
 
 void radixSort(int arr[], int n) {
-    // Find the maximum number to determine the number of digits
+    
     int max = arr[0];
     for (int i = 1; i < n; i++)
         if (arr[i] > max)
             max = arr[i];
 
-    // Do counting sort for every digit, starting from the least significant digit
+   
     for (int exp = 1; max / exp > 0; exp *= 10)
         countingSort(arr, n, exp);
 }
